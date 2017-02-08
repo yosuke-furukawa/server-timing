866508a2d87fab3673c28e48f4099b77eb6daf9b
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2017-02-09 07:54:42 +0900

    fix: headers may be leaked when raced

--- test/http-basic-test.js
+++ test/http-basic-test.js
@@ -42,3 +42,41 @@ test('http append more server timing response', () => {
     }))
   })
 })
+
+test('http request twice more server timing response', () => {
+  let count = 0;
+  const server = http.createServer((req, res) => {
+    serverTiming()(req, res)
+    if (count === 0) {
+      res.setMetric('foo', 100.0)
+      res.setMetric('bar', 10.0, 'Bar is not Foo')
+      res.setMetric('baz', 0)
+      res.end('hello')
+    }
+    if (count === 1) {
+      res.setMetric('test', 0.10, 'Test')
+      res.end('world')
+    }
+    count++
+  }).listen(0, () => {
+    http.get(`http://localhost:${server.address().port}/`, mustCall((res) => {
+      const assertStream = new AssertStream()
+      assertStream.expect('hello')
+      res.pipe(assertStream)
+
+      const timingHeader = res.headers['server-timing']
+      assert(/total=.*; "Total Response Time"/.test(timingHeader))
+      assert(/foo=100, bar=10; "Bar is not Foo", baz=0/.test(timingHeader))
+      http.get(`http://localhost:${server.address().port}/`, mustCall((res) => {
+        const assertStream = new AssertStream()
+        assertStream.expect('world')
+        res.pipe(assertStream)
+
+        const timingHeader = res.headers['server-timing']
+        assert(/total=.*; "Total Response Time"/.test(timingHeader))
+        assert(/test=0.1; "Test"/.test(timingHeader))
+        server.close()
+      }))
+    }))
+  })
+})
